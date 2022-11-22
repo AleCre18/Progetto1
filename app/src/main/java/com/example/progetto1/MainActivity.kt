797fab93c6f9c6d9733044f4edd0b7299f25ff8a
Alessandro <alecre18@gmail.com>
2022-11-22 22:44:20 +0100

    Prova

--- app/src/main/java/com/example/progetto1/MainActivity.kt
+++ app/src/main/java/com/example/progetto1/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.progetto1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.progetto1.ui.theme.Progetto1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +24,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Progetto1Theme {
+                val moneyCounter = remember {
+                    mutableStateOf(0)
+                }
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.Green) {
+                    //Greeting("Android")
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center) {
+                        Text(text = "${moneyCounter.value}$", fontSize = 35.sp, fontWeight = FontWeight.ExtraBold)
+                        Spacer(modifier = Modifier.padding(30.dp))
+                        CreateCircle(moneyCounter.value){
+                            moneyCounter.value += 1
+                        }
+                    }
                 }
             }
         }
@@ -27,14 +45,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!?")
-}
+fun CreateCircle(moneyCounter: Int = 0, addMoney: (Int) -> Unit ){
+    //var moneyCounter by remember {
+    //    mutableStateOf(0)
+    //}
 
-@Preview(name = "Anteprima progetto_",showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Progetto1Theme {
-        Greeting("Android")
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(100.dp)
+        .clickable {
+            addMoney(moneyCounter)
+            Log.d("Money", "createCircle: My balance: $moneyCounter dollars.")
+        },
+    shape = CircleShape, elevation = 5.dp
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap", modifier = Modifier)
+        }
     }
 }
